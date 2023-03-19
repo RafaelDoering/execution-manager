ae689e17d92600e2c3d8fe3cbd3840f1e8eaa0a2
rafaeldoering <rafael.doering.soares@usp.br>
2023-03-19 19:28:58 -0300

    Initial commit

---
+++ src/index.test.ts
@@ -0,0 +1,178 @@
+import ExecutionManager, { Context } from './index';
+
+test('execute should return nothing when called without add execution', async () => {
+  const subject = new ExecutionManager();
+
+  const result = await subject.execute();
+
+  expect(result).toStrictEqual({});
+});
+
+test('execute should return initial context when called without add execution', async () => {
+  const subject = new ExecutionManager({
+    test1: 1,
+  });
+
+  const result = await subject.execute();
+
+  expect(result).toStrictEqual({
+    test1: 1,
+  });
+});
+
+test('execute should return context with execution result when called after add execution', async () => {
+  const subject = new ExecutionManager({
+    test1: 1,
+  }).addExecution({
+    contextKey: 'test2',
+    call: () => {
+      return 2;
+    },
+  });
+
+  const result = await subject.execute();
+
+  expect(result).toStrictEqual({
+    test1: 1,
+    test2: 2,
+  });
+});
+
+test('execute should return context with execution result when called after add execution with promise', async () => {
+  const subject = new ExecutionManager({
+    test1: 1,
+  }).addExecution({
+    contextKey: 'test2',
+    call: () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(2);
+        }, 10);
+      });
+    },
+  });
+
+  const result = await subject.execute();
+
+  expect(result).toStrictEqual({
+    test1: 1,
+    test2: 2,
+  });
+});
+
+test('addExecution should pass context to call ', async () => {
+  const subject = new ExecutionManager({
+    test1: 1,
+  }).addExecution({
+    contextKey: 'test2',
+    call: (context: Context) => {
+      return {
+        result1: context.test1,
+        result2: 2,
+      };
+    },
+  });
+
+  const result = await subject.execute();
+
+  expect(result).toStrictEqual({
+    test1: 1,
+    test2: {
+      result1: 1,
+      result2: 2,
+    },
+  });
+});
+
+test('addExecution should throw error when called with a non existent dependency', async () => {
+  const subject = new ExecutionManager();
+
+  expect(() => subject.addExecution({
+    contextKey: 'test1',
+    call: () => {
+      return 1;
+    },
+    contextDependencies: ['test'],
+  })).toThrowError();
+});
+
+test('execute should return context with executions result when called after add execution with dependency', async () => {
+  const subject = new ExecutionManager({
+    test1: 1,
+  }).addExecution({
+    contextKey: 'test2',
+    call: () => {
+      return 2;
+    },
+  }).addExecution({
+    contextKey: 'test3',
+    call: (context: Context) => {
+      return {
+        result1: context.test1,
+        result2: context.test2,
+        result3: 3,
+      };
+    },
+    contextDependencies: ['test1', 'test2'],
+  });
+
+  const result = await subject.execute();
+
+  expect(result).toStrictEqual({
+    test1: 1,
+    test2: 2,
+    test3: {
+      result1: 1,
+      result2: 2,
+      result3: 3,
+    },
+  });
+});
+
+test('execute should return context with executions result when called after add execution with promises and dependencies', async () => {
+  const subject = new ExecutionManager().addExecution({
+    contextKey: 'test1',
+    call: () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(1);
+        }, 10);
+      });
+    },
+  }).addExecution({
+    contextKey: 'test2',
+    call: () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(2);
+        }, 15);
+      });
+    },
+  }).addExecution({
+    contextKey: 'test3',
+    call: (context: Context) => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({
+            result1: context.test1,
+            result2: context.test2,
+            result3: 3,
+          });
+        }, 5);
+      });
+    },
+    contextDependencies: ['test1', 'test2'],
+  });
+
+  const result = await subject.execute();
+
+  expect(result).toStrictEqual({
+    test1: 1,
+    test2: 2,
+    test3: {
+      result1: 1,
+      result2: 2,
+      result3: 3,
+    },
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,69 @@
+export type Context = { [key: string]: any };
+
+export type Execution = {
+  contextKey: string;
+  call: (context: Context) => any;
+  contextDependencies?: string[];
+};
+
+export default class ExecutionManager {
+  private context: Context = {};
+  private executionsByLevel: { [key: number]: Array<Execution> } = { 0: [] };
+
+  constructor(context?: Context) {
+    if (context) {
+      this.context = context;
+    }
+
+    return this;
+  }
+
+  public addExecution(execution: Execution): ExecutionManager {
+    if (execution.contextDependencies) {
+      const dependenciesLevel: Array<number> = [];
+      execution.contextDependencies.forEach((contextDependency: string) => {
+        const executionsWithLevel = Object.entries(this.executionsByLevel);
+        const dependency = executionsWithLevel.find(([level, executions]) => {
+          const foundDependency = executions.find((item) => item.contextKey === contextDependency);
+
+          if (foundDependency) {
+            dependenciesLevel.push(+level);
+          }
+
+          return foundDependency;
+        })
+
+        if (!dependency && !this.context[contextDependency]) {
+          throw 'Dependencia não encontrada';
+        }
+      });
+
+      const executionLevel = Math.max(...dependenciesLevel) + 1;
+
+      if (!this.executionsByLevel[executionLevel]) {
+        this.executionsByLevel[executionLevel] = [];
+      }
+
+      this.executionsByLevel[executionLevel].push(execution);
+    } else {
+      this.executionsByLevel[0].push(execution);
+    }
+
+
+    return this;
+  }
+
+  public async execute(): Promise<Context> {
+    const executionsByLevel = Object.values(this.executionsByLevel);
+
+    for (const executionInLevel of executionsByLevel) {
+      const responses = await Promise.all(executionInLevel.map(execution => execution.call(this.context)));
+
+      for (const [index, response] of responses.entries()) {
+        this.context[executionInLevel[index].contextKey] = response;
+      }
+    }
+
+    return this.context;
+  }
+}
