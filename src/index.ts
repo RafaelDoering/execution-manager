624e9d437e3c59c61350e16c5a95b5f137e5cbe2
rafaeldoering <rafael.doering.soares@usp.br>
2023-03-21 23:22:15 -0300

    Fix lint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export type Context = {[key: string]: unknown};
+export type Context = { [key: string]: unknown };
 
 export type Execution = {
   contextKey: string;
@@ -8,7 +8,7 @@ export type Execution = {
 
 export default class ExecutionManager {
   private context: Context = {};
-  private executionsByLevel: {[key: number]: Array<Execution>} = {0: []};
+  private executionsByLevel: { [key: number]: Array<Execution> } = { 0: [] };
 
   constructor(context?: Context) {
     if (context) {
@@ -25,7 +25,7 @@ export default class ExecutionManager {
         const executionsWithLevel = Object.entries(this.executionsByLevel);
         const dependency = executionsWithLevel.find(([level, executions]) => {
           const foundDependency = executions.find(
-            item => item.contextKey === contextDependency
+            (item) => item.contextKey === contextDependency
           );
 
           if (foundDependency) {
@@ -36,7 +36,7 @@ export default class ExecutionManager {
         });
 
         if (!dependency && !this.context[contextDependency]) {
-          throw 'Dependencia não encontrada';
+          throw "Dependencia não encontrada";
         }
       });
 
@@ -59,7 +59,7 @@ export default class ExecutionManager {
 
     for (const executionInLevel of executionsByLevel) {
       const responses = await Promise.all(
-        executionInLevel.map(execution => execution.call(this.context))
+        executionInLevel.map((execution) => execution.call(this.context))
       );
 
       for (const [index, response] of responses.entries()) {
